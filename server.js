d6c0650f183ee19b72acd6a618597e4c602c02dd
kngurley <kngurley25@gmail.com>
2021-12-12 13:12:26 -0600

    add login/logout functionality

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const path = require("path");
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+// express-session and sequelize store libraries
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +26,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
